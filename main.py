f24625ab6a95fe386f3fdbf10a033fed95fc049e
dazz-lynn <37522735+dazz-lynn@users.noreply.github.com>
2022-01-24 01:42:39 +0800

    Fixing main py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from src import ner_visualizer, dependency_visualizer, home_page
+from src import ner_visualizer, dependency_visualizer, home_page, pos_tagger
 
 pages = {
     "Introduction": home_page,
+    "POS Tagging": pos_tagger,
     "Named Entity Recognition": ner_visualizer,
     "Token Analysis & Dependency Visualizer": dependency_visualizer,
 }
