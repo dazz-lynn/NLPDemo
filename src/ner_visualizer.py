78dc3d8b0627f9e4ff7991022c158f11a7c2ee87
dazz-lynn <37522735+dazz-lynn@users.noreply.github.com>
2022-01-23 23:46:29 +0800

    NER add NLTK fix 6

--- src/ner_visualizer.py
+++ src/ner_visualizer.py
@@ -34,5 +34,7 @@ def app():
         for sent in nltk.sent_tokenize(text):
             for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
                 if hasattr(chunk, 'label'):
+                    if chunk.label() == 'GPE':
+                        st.write(st.text('GEOPOLITICAL'), ' '.join(c[0] for c in chunk))
                     st.write(chunk.label(), ' '. join(c[0] for c in chunk))
 
