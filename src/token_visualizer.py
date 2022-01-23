775b274c1330926cf25548781dae1d2e47c915eb
dazz-lynn <37522735+dazz-lynn@users.noreply.github.com>
2022-01-24 02:46:32 +0800

    Changes to UI

--- src/token_visualizer.py
+++ src/token_visualizer.py
@@ -2,16 +2,30 @@ import spacy_streamlit
 import streamlit as st
 import spacy
 import nltk
+import webbrowser
 from .utils import load_data_pandas, load_model, load_data_json
 
 def app():
+    st.title("Tokenization")
+    with st.expander("See explanation"):
+        add_desc_ = st.markdown('Tokenization is a way of separating a piece of text into smaller units called '
+                                        'tokens. Here, tokens can be either words, characters, or subwords. Hence, '
+                                        'tokenization can be broadly classified into 3 types – word, character, '
+                                        'and subword (n-gram characters) tokenization. It is a fundamental step in both '
+                                        'traditional and deep-learning NLP pipelines.')
+        url = 'https://spacy.io/api/tokenizer'
+        if st.button('spaCy'):
+            webbrowser.open_new_tab(url)
+
     model_name = "en_core_web_sm"
     packages = ["spaCy", "NLTK"]
-
     nlp = spacy.load(model_name)
 
-    st.title("Tokenization")
-    st.subheader("What type of NLP package would like to explore?")
+    col1, col2 = st.columns([2, 1])
+    with col1:
+        st.subheader("What type of NLP package would you like to explore?")
+    with col2:
+        st.checkbox("Display side by side")
     pkg_text = st.selectbox("NLP package:", packages)
     st.markdown("---")
     st.subheader("Enter the text you'd like to analyze.")
